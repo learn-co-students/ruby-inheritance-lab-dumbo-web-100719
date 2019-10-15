e07dff1cecc95ef47537e0e2b59b72067d52fbb8
Diego Martinez <Diego@Diegos-MacBook-Pro.local>
2019-10-15 10:54:20 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,15 @@
 class Student  < User
+    attr_accessor :knowledge
 
     def initialize
         @knowledge = []
     end
 
     def learn(something)
-        knowledge <<  something
+        @knowledge <<  something
     end
 
     def knowledge
-        knowledge
-    
+        @knowledge
+    end
 end
